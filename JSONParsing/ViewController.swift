957f1eb35b392613688331cacdfb7c32c5889801
Yongxiang Ruan <edwinruan@gmail.com>
2018-12-11 20:17:37 -0600

    Initial Commit

---
+++ JSONParsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONParsing
+//
+//  Created by Rush01 on 12/11/18.
+//  Copyright © 2018 Rush01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
